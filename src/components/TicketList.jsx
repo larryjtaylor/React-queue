4fd90fd6a98eeefd88909684fc81c227e6c7f33f
Larry <larryjamestaylor@gmail.com>
2017-09-10 23:14:03 -0700

    build project to weekend end point

---
+++ src/components/TicketList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Ticket from "./Ticket";
+
+function TicketList(props){
+  return (
+    <Ticket
+      location='7c'
+      names='Larry'
+      issue='stuff is broke'/>
+  );
+}
+
+export default TicketList;
